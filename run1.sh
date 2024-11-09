1338b5d74e43670cfb6b2ea4556e7771dab9f883
winbo-yml-exe <winbo-yml-exe@users.noreply.github.com>
2024-11-09 06:32:38 +0000

    Update run.sh with commands

--- run1.sh
+++ run1.sh
@@ -1,4 +1,4 @@
-cd RisingOS-
-repo init -u https://github.com/RisingTechOSS/android -b fifteen --git-lfs --depth=1
+cd CipherOS-
+repo init -u https://github.com/CipherOS/android_manifest.git -b fifteen --git-lfs --depth=1
 chmod +x craverun.sh
 . craverun.sh
