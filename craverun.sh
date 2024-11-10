f76c1a5ba64f8dde824f6690a21715de1ad70e2b
winbo-yml-exe <winbo-yml-exe@users.noreply.github.com>
2024-11-10 07:42:42 +0000

    Update run.sh with commands

--- craverun.sh
+++ craverun.sh
@@ -3,7 +3,7 @@ rm -rf crave_rom_builder
 git clone https://$GH_TOKEN@github.com/winbo-yml-exe/crave_rom_builder.git
 cp -f crave_rom_builder/pull.sh pull.sh
 chmod +x pull.sh
-crave run --no-patch  -- "          #© crave.io Inc. 2017-2024
+crave run --no-patch --clean -- "          #© crave.io Inc. 2017-2024
               #Thanks to UV n Team
         #no dual account just to build faster
               #respect the rule
@@ -13,7 +13,7 @@ git clone https://github.com/winbo-yml-exe/local_manifests -b main .repo/local_m
 /opt/crave/resync.sh
  
 source build/envsetup.sh
-lunch lineage_gale-userdebug
+lunch cipher_gale-userdebug
 m installclean
 brunch gale
 "
