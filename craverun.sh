e66de0bc36af1050eb41e13728a24f7f440ec3ca
winbo-yml-exe <winbo-yml-exe@users.noreply.github.com>
2024-11-24 08:11:52 +0000

    Update run.sh with commands

--- craverun.sh
+++ craverun.sh
@@ -3,11 +3,11 @@ rm -rf crave_rom_builder
 git clone https://$GH_TOKEN@github.com/winbo-yml-exe/crave_rom_builder.git
 cp -f crave_rom_builder/pull.sh pull.sh
 chmod +x pull.sh
-crave run --no-patch --clean -- "          #© crave.io Inc. 2017-2024
+crave run --no-patch  -- "          #© crave.io Inc. 2017-2024
               #Thanks to UV n Team
         #no dual account just to build faster
               #respect the rule
-
+repo init -u https://github.com/alphadroid-project/manifest -b alpha-14 --git-lfs --depth=1
 rm -rf .repo/local_manifests 
 git clone https://github.com/winbo-yml-exe/local_manifests -b main .repo/local_manifests
 /opt/crave/resync.sh
--- run1.sh
+++ run1.sh
@@ -1,4 +1,4 @@
-cd AlphaDroid-
-repo init -u https://github.com/AlphaDroid-Project/manifest.git -b alpha-14 --git-lfs --depth=1
+cd LineageOS21-
+repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs --depth=1
 chmod +x craverun.sh
 . craverun.sh
